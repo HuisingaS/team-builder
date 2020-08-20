b161ecad3de4b1d757b995dfb8f4ea2108ce2796
Stephen Huisinga <stephenhuisinga@gmail.com>
2020-08-20 09:12:31 -0500

    MVP complete - minor CSS tweak

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import uuid from "uuid";
+
+const initialTeamList = [
+  {
+    name: "Billy-Bob",
+    id: "1",
+    email: "astronaut@astronautsRus.spaceforce",
+    role: "Lead Space Walker Guy"
+  }
+];
+
+const initialTeamForm = {
+  name: "",
+  email: "",
+  role: ""
+};
+
+function App() {
+  const [teamList, setTeamList] = useState(initialTeamList);
+  const [teamForm, setTeamForm] = useState(initialTeamForm);
+
+  const onNameChange = e => {
+    setTeamForm({
+      name: e.target.value,
+      email: teamForm.email,
+      role: teamForm.role
+    });
+  };
+
+  const onEmailChange = e => {
+    setTeamForm({
+      name: teamForm.name,
+      email: e.target.value,
+      role: teamForm.role
+    });
+  };
+
+  const onRoleChange = e => {
+    setTeamForm({
+      name: teamForm.name,
+      email: teamForm.email,
+      role: e.target.value
+    });
+  };
+
+  const onFormSubmit = event => {
+    event.preventDefault();
+    const newTeamMember = {
+      id: uuid(),
+      name: teamForm.name,
+      email: teamForm.email,
+      role: teamForm.role
+    };
+    const newTeamList = teamList.concat(newTeamMember);
+    setTeamList(newTeamList);
+    setTeamForm(initialTeamForm);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Join our team...please!</h1>
+        <Form
+          onNameChange={onNameChange}
+          onEmailChange={onEmailChange}
+          onRoleChange={onRoleChange}
+          teamForm={teamForm}
+          onFormSubmit={onFormSubmit}
+        />
+        {teamList.map(teamMember => (
+          <div key={teamMember.id}>
+            <p>Name: {teamMember.name}</p>
+            <p>Email: {teamMember.email}</p>
+            <p>Role: {teamMember.role}</p>
+          </div>
+        ))}
+      </header>
+    </div>
+  );
+};
+
+export default App;
